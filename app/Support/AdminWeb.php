4847a3bbe618a36ef24e10c16a940b4240f82728
wuxiaoyong <851780358@qq.com>
2026-06-03 13:29:46 +0800

    fix  修改管理端品牌名称

--- app/Support/AdminWeb.php
+++ app/Support/AdminWeb.php
@@ -23,7 +23,7 @@ final class AdminWeb
 
     public static function siteName(): string
     {
-        return '策影GEO';
+        return 'AI生成引擎优化师';
     }
 
     public static function basePath(): string
